03567778953f6fe643959829c0755cafd4409464
Joe Stephens <bodynsoulgraphix@gmail.com>
2019-12-21 11:11:39 -0600

    Fix hour active state

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = ({
-      hour: 12,
+      hour: 24,
       minute: 0,
       second: 0,
       ampm: ''
     });
   }
 
-  componentWillMount() {
+  componentDidMount() {
     setInterval(() => {
       let time = new Date();
       this.setState({
         hour: time.getHours(),
         minute: time.getMinutes(),
         second: time.getSeconds(),
-        ampm: time >= 12 ? 'pm' : 'am'
-
+        ampm: time >= 12 ? 'am' : 'pm'
       });
     }, 1000);
   }
 
   // Map out getHours and move ahead 1 in array...
-  hourArray = length => Array.from({ length }).map((v, k) => k).map(x => x + 1);
+  hourArray = length => Array.from({ length }).map((v, k) => k).map(x => x+1);
+  
   // Map out getMinutes and getSeconds display '0' if < 10
   array = length => Array.from({ length }).map((v, k) => k).map(x => x).map(x => x < 10 ? '0' + x : x);
   
@@ -34,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className={`App-header ${(this.state.ampm) ? "pm" : "am"}`}>          
+        <header className={`App-header ${(this.state.ampm) ? "am" : "pm"}`}>          
           <div className='box'>
             <div className='box-inner'>
             <div className='ampm'>
@@ -44,9 +44,8 @@ class App extends Component {
               {this.hourArray(1).map((x, index) => {
                 return (
                   <div key={index}
-                    className={`hour earth ${index === (this.state.hour - 13) ? "active" : ""}`}
-                    style={{ animation: `rotate 86400s linear infinite`, transition: `transform 86400000ms linear`, transform: `translate(-50%, -50%) rotate(${index * (360 / 1) - (360 / 1) * (this.state.hour - 13)}deg)` }}>
-                    {`${x}`}
+                    className={`hour earth ${index === (this.state.hour - 1) ? "active" : ""}`}
+                    style={{ animation: `rotate 86400s linear infinite`, transition: `transform 86400000ms linear`, transform: `translate(-50%, -50%) rotate(${index * (360 / 1) - (360 / 1) * (this.state.hour - 1)}deg)` }}>
                   </div>
                 )
               })}
@@ -54,8 +53,8 @@ class App extends Component {
             {this.hourArray(12).map((x, index) => {
               return (
                 <div key={index}
-                  className={`hour num ${index === (this.state.hour - 13) ? "active" : ""}`}
-                  style={{ transform: `translate(-50%, -50%) rotate(${index * (360 / 12) - (360 / 12) * (this.state.hour - 13)}deg)` }}>
+                  className={`hour item ${index === (this.state.hour - 1) ? "active" : ""}`}
+                  style={{ transform: `translate(-50%, -50%) rotate(${index * (360 / 12) - (360 / 12) * (this.state.hour - 1)}deg)` }}>
                   {`${x}`}
                 </div>
               )
@@ -64,7 +63,7 @@ class App extends Component {
               {this.array(60).map((x, index) => {
               return (
                 <div key={index}
-                  className={`minute num ${index === (this.state.minute) ? "active" : ""}`}
+                  className={`minute item ${index === (this.state.minute) ? "active" : ""}`}
                   style={{ transform: `translate(-50%, -50%) rotate(${index * (360 / 60) - (360 / 60) * (this.state.minute)}deg)` }}>
                   {`${x}`}
                 </div>
@@ -74,7 +73,7 @@ class App extends Component {
             {this.array(60).map((x, index) => {
               return (
                 <div key={index}
-                  className={`second num ${index === (this.state.second) ? "active" : ""}`}
+                  className={`second item ${index === (this.state.second) ? "active" : ""}`}
                   style={{ transform: `translate(-50%, -50%) rotate(${index * (360 / 60) - (360 / 60) * (this.state.second)}deg)` }}>
                   {`${x}`}
                 </div>
